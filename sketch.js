4dc2d62ca178113d6ebd7d31ea2c7d5bfd44e005
bhakti <bhaktikalyani951@gmail.com>
2021-02-12 16:34:48 +0530

    pig 3 has been removed

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+   
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -52,7 +52,7 @@ function draw(){
 
     box3.display();
     box4.display();
-    pig3.display();
+   
     log3.display();
 
     box5.display();
